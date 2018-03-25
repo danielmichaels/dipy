600aff6a78289a122cd92c1af6f300493f9fc1d6
Dan <dans.address@outlook.com>
2018-03-25 20:27:01 +0000

    Add docstrings to class Weather. Replace prints with Logging.

--- weather.py
+++ weather.py
@@ -1,13 +1,17 @@
 from api_secrets import WEATHER_TOKEN
 from utils import datetime_helper
-from pprint import pprint
 import requests
+import requests_cache
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
 
 class Weather:
+    """A simple class that pulls in weather data from the openweathermap api
+    and formats the response into Discord readable markdown.
+
+    """
 
     def __init__(self, city='perth', country='AU'):
         self.city = city
@@ -17,8 +21,12 @@ class Weather:
             city=self.city, country=self.country, TOKEN=WEATHER_TOKEN)
 
     def get_data(self):
-        """Get the weather data in JSON format."""
-        print(self.url)
+        """Get the weather data in JSON format. Caches the response for
+        600 seconds in a sqlite database.
+
+        """
+        logging.info(self.url)
+        requests_cache.install_cache(expire_after=600)
         response = requests.get(self.url)
         if response.status_code != 200:
             logging.error("API did not return status code of 200")
@@ -28,8 +36,11 @@ class Weather:
     def format(self, data):
         """Parse out wanted weather info.
 
-        :arg data, required from :meth get_data"""
-        # pprint(data)
+        :arg data: required from get_data method.
+
+        :return output in markdown.
+
+        """
         location, cc = data['name'], data['sys']['country']
         temp = data['main']['temp']
         conditions = [item['description'] for item in data['weather']][0]
@@ -43,7 +54,3 @@ class Weather:
         As At:\t **{3}**
         """.format(location, temp, conditions, local_time, cc)
         return output
-
-# weather = Weather()
-# data = weather.get_data()
-# weather.format(data)
