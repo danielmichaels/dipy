1e98415fce95088ee82d92bf503c26ea4f0d335a
danielmichaels <dans.address@outlook.com>
2019-03-31 14:07:46 +1100

    Cleanup previous commands for Rewrite conversion

--- program.py
+++ program.py
@@ -1,15 +1,17 @@
+import logging
+from datetime import datetime
+
 import discord
+import requests
+from discord.ext import commands
 
 from api_secrets import BOT_TOKEN
-from datetime import datetime
-from discord.ext import commands
 from weather import Weather
-import logging
-import requests
 
 logging.basicConfig(level=logging.INFO)
 
 bot = commands.Bot(command_prefix='?', description='A simple tutbot')
+bot.remove_command('help')
 
 
 @bot.event
@@ -24,34 +26,39 @@ async def hello(ctx):
     await ctx.send('World!')
 
 
+@bot.command(description="Given two integers return their sum.")
+async def add(ctx, int1: int, int2: int):
+    await ctx.send(int1 + int2)
+
+
 @bot.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a + b)
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
 
 
-# @bot.command(description="Prints servers system time")
-# async def time():
-#    now = datetime.ctime(datetime.now())
-#    await bot.say(now)
-#
-#
-# @bot.command(description="Prints out current CoinDesk USD value of BTC")
-# async def btc():
-#    btc_now = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
-#    js = requests.get(btc_now).json()
-#    usd_price = js['bpi']['USD']['rate']
-#    await bot.say('BTC rate in USD now: {}'.format(usd_price))
-#
-#
-# @bot.command(
-#    description="Get weather from user entered location: requires {city} {country code}"
-#                "\n\n If city is hyphenated please use underscores: alice_springs")
-# async def weather(city='perth', cc='au'):
-#    city = city.replace("_", " ")
-#    query = Weather(city, cc)
-#    data = query.get_data()
-#    results = query.format(data)
-#    await bot.say(results)
+@bot.command(description="Prints servers system time")
+async def time(ctx):
+    now = datetime.ctime(datetime.now())
+    await ctx.send(now)
+
+
+@bot.command(description="Prints out current CoinDesk USD value of BTC")
+async def btc(ctx):
+    btc_now = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+    js = requests.get(btc_now).json()
+    usd_price = js['bpi']['USD']['rate']
+    await ctx.send('BTC rate in USD now: {}'.format(usd_price))
+
+
+@bot.command(
+    description="Get weather from user entered location: requires {city} {country code}"
+                "\n\n If city is hyphenated please use underscores: alice_springs")
+async def weather(ctx, city='newcastle', cc='au'):
+    city = city.replace("_", " ")
+    query = Weather(city, cc)
+    data = query.get_data()
+    results = query.format(data)
+    await ctx.send(results)
 
 
 bot.run(BOT_TOKEN)
